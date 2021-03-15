fcaf022414bda718256ea3f07eafe5b9d222fbf7
Shweta Boddupalli <sboddupalli@unomaha.edu>
2021-03-14 21:24:24 -0500

    Initial Commit
    Initial Commit

---
+++ vue_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\shweta\desktop\mis\8380 enterprise\assignment3\efs_rest\vue_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
